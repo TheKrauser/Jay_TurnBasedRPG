998a00351ea82658e09761563c18d6ea88993fae
Kevin <kevincaireleandro@hotmail.com>
2023-11-14 23:27:39 -0300

    New Menu and Characters Sprite Changed

---
+++ Jay_TurnBasedRPG/Assets/_Scripts/MenuManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup menu, options, stage;
+
+    public void Play()
+    {
+        ManageView(menu, false);
+        ManageView(stage, true);
+    }
+
+    public void SelectStage()
+    {
+        SceneManager.LoadScene("BattleScene");
+    }
+
+    public void Back()
+    {
+        ManageView(stage, false);
+        ManageView(menu, true);
+    }
+
+    public void Options()
+    {
+        ManageView(menu, false);
+        ManageView(options, true);
+    }
+
+    public void OptionsOK()
+    {
+        ManageView(options, false);
+        ManageView(menu, true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void ManageView(CanvasGroup group, bool enable)
+    {
+        if (enable)
+        {
+            group.alpha = 1;
+            group.interactable = true;
+            group.blocksRaycasts = true;
+        }
+        else
+        {
+            group.alpha = 0;
+            group.interactable = false;
+            group.blocksRaycasts = false;
+        }
+    }
+}
